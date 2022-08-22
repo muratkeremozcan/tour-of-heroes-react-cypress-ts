f21e66548854e4cd212504643e6bb90b24780176
Murat Ozcan <muratkerem@gmail.com>
2022-08-22 08:09:18 -0500

    headerBarBrand component

---
+++ src/components/HeaderBarBrand.tsx
@@ -0,0 +1,22 @@
+import {FaReact} from 'react-icons/fa'
+import {NavLink} from 'react-router-dom'
+
+export default function HeaderBarBrand() {
+  return (
+    <div data-cy="navbar" className="navbar-brand">
+      <a
+        href="https://reactjs.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="navbar-item"
+      >
+        <FaReact />
+      </a>
+      <NavLink data-cy="navLink" to="/" className="navbar-item navbar-home">
+        <span className="tour">TOUR</span>
+        <span className="of">OF</span>
+        <span className="heroes">HEROES</span>
+      </NavLink>
+    </div>
+  )
+}
