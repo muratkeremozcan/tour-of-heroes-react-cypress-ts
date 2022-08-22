908b5ea8e69843c43c352e18c8eb64e983d8ddbd
Murat Ozcan <muratkerem@gmail.com>
2022-08-22 08:19:44 -0500

    spacing between it block [ci skip]

--- src/components/HeaderBarBrand.cy.tsx
+++ src/components/HeaderBarBrand.cy.tsx
@@ -10,6 +10,7 @@ describe('HeaderBarBrand', () => {
       </BrowserRouter>,
     )
   })
+
   it('should verify external link attributes', () => {
     cy.get('a')
       .should('have.attr', 'href', 'https://reactjs.org/')
@@ -17,7 +18,8 @@ describe('HeaderBarBrand', () => {
       .and('have.attr', 'rel', 'noopener noreferrer')
     cy.getByCy('navbar').within(() => cy.get('svg'))
   })
-  it('should verify internal link navigation', () => {
+
+  it('should verify internal link spans and navigation', () => {
     cy.getByCy('navLink').within(() =>
       cy
         .wrap(['TOUR', 'OF', 'HEROES'])
