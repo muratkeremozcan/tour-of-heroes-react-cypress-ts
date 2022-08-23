6423c29e0c1c339956e2bb3e19347956d9c0263e
Murat Ozcan <muratkerem@gmail.com>
2022-08-23 08:09:22 -0500

    ListHeader component

---
+++ src/components/ListHeader.cy.tsx
@@ -0,0 +1,27 @@
+import ListHeader from './ListHeader'
+import {BrowserRouter} from 'react-router-dom'
+import '../styles.scss'
+
+describe('ListHeader', () => {
+  it('should call click handlers on add & refresh button clicks', () => {
+    const title = 'Heroes'
+    cy.mount(
+      <BrowserRouter>
+        <ListHeader
+          title={title}
+          handleAdd={cy.stub().as('handleAdd')}
+          handleRefresh={cy.stub().as('handleRefresh')}
+        />
+      </BrowserRouter>,
+    )
+
+    cy.getByCy('add-button').click()
+    cy.get('@handleAdd').should('be.called')
+
+    cy.getByCy('refresh-button').click()
+    cy.get('@handleRefresh').should('be.called')
+
+    cy.getByCy('title').contains(title).click()
+    cy.url().should('contain', title)
+  })
+})
