9e811888847a06d43f8b58d2ad3a25a18ea238a8
Murat Ozcan <muratkerem@gmail.com>
2022-08-23 13:27:02 -0500

    ModalYesNo

---
+++ src/components/Modal.cy.tsx
@@ -0,0 +1,49 @@
+import ModalYesNo from './ModalYesNo'
+import '../styles.scss'
+
+describe('ModalYesNo', () => {
+  it('should render the modal and call onClick handlers', () => {
+    const message = 'Are you sure?'
+    cy.mount(
+      <ModalYesNo
+        message={message}
+        onYes={cy.stub().as('onYes')}
+        onNo={cy.stub().as('onNo')}
+      />,
+    )
+
+    cy.get('#modal-root').should('exist')
+
+    cy.get('div').within(() => {
+      cy.get('header').contains('Confirm')
+      cy.get('section').contains(message)
+      cy.get('footer')
+      cy.getByCy('button-yes').contains('Yes')
+      cy.getByCy('button-no').contains('No')
+    })
+
+    cy.getByCy('button-yes').click()
+    cy.get('@onYes').should('be.called')
+
+    cy.getByCy('button-no').click()
+    cy.get('@onNo').should('be.called')
+  })
+
+  it('should not render the modal with if conditional render is false', () => {
+    function ParentComponent(): JSX.Element | boolean {
+      return (
+        false && (
+          <ModalYesNo
+            message={'yo'}
+            onYes={cy.stub().as('onYes')}
+            onNo={cy.stub().as('onNo')}
+          />
+        )
+      )
+    }
+
+    // @ts-expect-error: replicating useState
+    cy.mount(<ParentComponent />)
+    cy.getByCy('modal-yes-no').should('not.exist')
+  })
+})
---
+++ src/components/ModalYesNo.tsx
@@ -0,0 +1,40 @@
+import Modal from './Modal'
+import {SyntheticEvent} from 'react'
+
+type ModalYesNoProps = {
+  message: string
+  onYes: (e: SyntheticEvent) => void
+  onNo: (e: SyntheticEvent) => void
+}
+
+export default function ModalYesNo({message, onYes, onNo}: ModalYesNoProps) {
+  return (
+    <Modal>
+      <div data-cy="modal-yes-no" className="modal is-active">
+        <div className="modal-background" />
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">Confirm</p>
+          </header>
+          <section className="modal-card-body">{message}</section>
+          <footer className="modal-card-foot card-footer">
+            <button
+              data-cy="button-no"
+              onClick={onNo}
+              className="button modal-no"
+            >
+              No
+            </button>
+            <button
+              data-cy="button-yes"
+              onClick={onYes}
+              className="button is-primary modal-yes"
+            >
+              Yes
+            </button>
+          </footer>
+        </div>
+      </div>
+    </Modal>
+  )
+}
