ebe617c3058f2f517dfd5757b6c81c20bf7b931c
Murat Ozcan <muratkerem@gmail.com>
2022-08-28 06:59:57 -0500

    updated sanity component test

--- src/App.cy.tsx
+++ src/App.cy.tsx
@@ -3,6 +3,12 @@ import App from './App'
 describe('ct sanity', () => {
   it('should render the App', () => {
     cy.mount(<App />)
-    cy.getByCy('greeting').should('be.visible')
+    cy.getByCy('not-found').should('be.visible')
+
+    cy.get(`[href="/heroes"]`).click()
+    cy.getByCy('heroes').should('be.visible')
+
+    cy.get(`[href="/about"]`).click()
+    cy.getByCy('about').should('be.visible')
   })
 })
