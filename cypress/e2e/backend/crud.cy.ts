1474f698103d730f088504ce66910647f976c334
Murat Ozcan <muratkerem@gmail.com>
2022-08-30 06:29:38 -0500

    added json-server and backend api e2e tests

---
+++ cypress/e2e/backend/crud.cy.ts
@@ -0,0 +1,46 @@
+import type {Hero} from '../../support/commands'
+
+describe('Backend e2e', () => {
+  const assertProperties = (entity: Hero) => {
+    expect(entity.id).to.be.a('string')
+    expect(entity.name).to.be.a('string')
+    expect(entity.description).to.be.a('string')
+  }
+
+  beforeEach(() => cy.resetData())
+
+  it('should GET heroes and villains ', () => {
+    cy.crud('GET', 'heroes')
+      .its('body')
+      .should('have.length.gt', 0)
+      .each(assertProperties)
+
+    cy.crud('GET', 'villains')
+      .its('body')
+      .should('have.length.gt', 0)
+      .each(assertProperties)
+  })
+
+  it('should CRUD a new hero entity', () => {
+    const newHero = {id: 'Ragnarok', name: 'Ragnar', description: 'Lothbrok'}
+
+    cy.crud('POST', 'heroes', {body: newHero})
+
+    cy.crud('GET', 'heroes')
+      .its('body')
+      .then(body => {
+        expect(body.at(-1)).to.deep.eq(newHero)
+      })
+
+    const editedHero = {...newHero, name: 'Murat'}
+    cy.crud('PUT', `heroes/${editedHero.id}`, {body: editedHero})
+    cy.crud('GET', `heroes/${editedHero.id}`)
+      .its('body')
+      .should('deep.eq', editedHero)
+
+    cy.crud('DELETE', `heroes/${editedHero.id}`)
+    cy.crud('GET', `heroes/${editedHero.id}`, {allowedToFail: true})
+      .its('status')
+      .should('eq', 404)
+  })
+})
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,5 @@
 import '@testing-library/cypress/add-commands'
+import data from '../fixtures/db.json'
 
 Cypress.Commands.add('getByCy', (selector, ...args) =>
   cy.get(`[data-cy="${selector}"]`, ...args),
@@ -11,3 +12,25 @@ Cypress.Commands.add('getByCyLike', (selector, ...args) =>
 Cypress.Commands.add('getByClassLike', (selector, ...args) =>
   cy.get(`[class*=${selector}]`, ...args),
 )
+
+export type Hero = {id: string; name: string; description: string}
+Cypress.Commands.add(
+  'crud',
+  (
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+    route: string,
+    {
+      body,
+      allowedToFail = false,
+    }: {body?: Hero | object; allowedToFail?: boolean} = {},
+  ) =>
+    cy.request<Hero[] & Hero>({
+      method: method,
+      url: `http://localhost:4000/api/${route}`,
+      body: method === 'POST' || method === 'PUT' ? body : undefined,
+      retryOnStatusCodeFailure: !allowedToFail,
+      failOnStatusCode: !allowedToFail,
+    }),
+)
+
+Cypress.Commands.add('resetData', () => cy.crud('POST', 'reset', {body: data}))
