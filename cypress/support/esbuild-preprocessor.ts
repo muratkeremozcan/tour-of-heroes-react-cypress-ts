2e9e0c2e8e0f3c16b9f5ea7e38055c29088f0116
Murat Ozcan <muratkerem@gmail.com>
2023-02-27 14:33:02 -0600

    esbuild * import

--- cypress/support/esbuild-preprocessor.ts
+++ cypress/support/esbuild-preprocessor.ts
@@ -1,4 +1,4 @@
-import createBundler from '@bahmutov/cypress-esbuild-preprocessor'
+import * as createBundler from '@bahmutov/cypress-esbuild-preprocessor'
 import {NodeGlobalsPolyfillPlugin} from '@esbuild-plugins/node-globals-polyfill'
 import {NodeModulesPolyfillPlugin} from '@esbuild-plugins/node-modules-polyfill'
 
