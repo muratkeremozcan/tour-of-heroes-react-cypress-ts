9bf156358c0401585ba47d51cbbf629ffdcaddb7
Murat Ozcan <muratkerem@gmail.com>
2023-02-27 14:03:47 -0600

    cy 12.7.0

--- cypress/support/esbuild-preprocessor.ts
+++ cypress/support/esbuild-preprocessor.ts
@@ -1,4 +1,4 @@
-import createBundler from '@bahmutov/cypress-esbuild-preprocessor'
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
 import {NodeGlobalsPolyfillPlugin} from '@esbuild-plugins/node-globals-polyfill'
 import {NodeModulesPolyfillPlugin} from '@esbuild-plugins/node-modules-polyfill'
 
