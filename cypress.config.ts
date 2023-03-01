ae9d686a0c1b879388f1deb84198baaf52a230f0
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-03-01 10:14:21 +1000

    update

--- cypress.config.ts
+++ cypress.config.ts
@@ -33,48 +33,8 @@ export default defineConfig({
     },
     specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}',
     devServer: {
-      framework: 'create-react-app',
-      bundler: 'webpack',
-      // here are the additional settings from Gleb's instructions
-      webpackConfig: {
-        resolve: {
-          alias: {
-            '@cypress': path.resolve(__dirname, 'cypress'),
-            '@support': path.resolve(__dirname, 'cypress', 'support'),
-            '@fixtures': path.resolve(__dirname, 'cypress', 'fixtures'),
-          },
-        },
-        // workaround to react-scripts 5 issue https://github.com/cypress-io/cypress/issues/22762
-        devServer: {
-          port: 3001,
-        },
-        mode: 'development',
-        devtool: false,
-        module: {
-          rules: [
-            // application and Cypress files are bundled like React components
-            // and instrumented using the babel-plugin-istanbul
-            {
-              test: /\.ts$/,
-              exclude: /node_modules/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: [
-                    '@babel/preset-env',
-                    '@babel/preset-react',
-                    '@babel/preset-typescript',
-                  ],
-                  plugins: [
-                    'istanbul',
-                    ['@babel/plugin-transform-modules-commonjs', {loose: true}],
-                  ],
-                },
-              },
-            },
-          ],
-        },
-      },
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 })
