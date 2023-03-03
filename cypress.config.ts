f091b022d4ef54529e60b28a844fad6d1576ae71
Murat Ozcan <muratkerem@gmail.com>
2023-03-03 12:14:30 -0600

    changed app port to 5173

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,6 @@ import {defineConfig} from 'cypress'
 import plugins from './cypress/support/plugins'
 import tasks from './cypress/support/tasks'
 import esbuildPreprocessor from './cypress/support/esbuild-preprocessor'
-import path from 'path'
 
 export default defineConfig({
   projectId: '7mypio',
@@ -18,9 +17,9 @@ export default defineConfig({
   },
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:5173',
     setupNodeEvents(on, config) {
-      esbuildPreprocessor(on)
+      esbuildPreprocessor(on) // no need with vite
       tasks(on)
       return plugins(on, config)
     },
